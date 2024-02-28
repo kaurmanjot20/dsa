1df86fe8274aedeb8c00c501e92fad175f63268b
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-02-28 10:59:53 +0530

    count using hashing

---
+++ hashing/countNum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int a[11]={1,3,4,5,3,4,2,7,8,1,0};
+    int hash[12]={0};
+    
+    //pre-calculate
+    for(int i=0;i<sizeof(a);i++){
+        hash[a[i]]+=1;
+    }
+
+    //fetch
+    cout << "Enter no. of inputs";
+    int q;
+    cin >> q;
+    cout << "Enter numbers to get their count";
+    int num;
+    while (q--){
+        cin >> num;
+        cout << hash[num];
+    }
+     
+
+}
