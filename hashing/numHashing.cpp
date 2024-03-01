03388e9d623c32cb18340ef93ea82674c24a5ca9
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-03-01 23:27:43 +0530

    num hashing using map

--- hashing/numHashing.cpp
+++ hashing/numHashing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
 int main(){
     int a[11]={1,3,4,5,3,4,2,7,8,1,0};
     int hash[12]={0};
@@ -20,6 +21,39 @@ int main(){
         cin >> num;
         cout << hash[num] << endl;
     }
-     
-
+    return 0;
+}
+*/
+#include <vector>
+#include <map>
+int main(){
+    cout << "Enter integer array size";
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    cout << "Enter the array";
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    //pre compute
+    map<int, int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[v[i]]++;
+    }
+    /*
+    iterate in map
+    for(auto it : mpp){
+        cout << it.first << "->" << it.second << endl;
+    }
+    */
+    int q;
+    cout << "Enter no. of queries";
+    cin >> q;
+    while(q--){
+        int num;
+        cout << "Enter the query " << endl;
+        cin >> num;
+        //fetch
+        cout << mpp[num] << endl;
+    }
 }
