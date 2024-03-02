5ed0476c144f81debd0d681e279b3fb8a6b2ac34
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-03-02 21:54:04 +0530

    char hashing using maps

--- hashing/charHashing.cpp
+++ hashing/charHashing.cpp
@@ -27,17 +27,16 @@ int main(){
 }
 */
 
-//no assumption; consider all 256 elements
+/*
+no assumption; consider all 256 elements
 int main(){
     string s;
     cout << "Enter a string";
     cin >> s;
-
     int hash[256] = {0};
     for(int i=0; i<s.size();i++){
         hash[s[i]]++;
     }
-
     int q;
     cout << "Enter no. of queries ";
     cin >> q;
@@ -47,6 +46,29 @@ int main(){
         cin >> c;
         //fetch
         cout << "Appears " << hash[c] << " times" << endl;
+    }
+    return 0;
+}
+*/
+// using map
+#include <map>
+int main(){
+    string s;
+    cout << "Enter a string";
+    cin >> s;
+    map<char,int> mpp;
+     for(int i=0;i<s.size();i++){
+        mpp[s[i]]++;
+    }
+    int q;
+    cout << "Enter no. of queries";
+    cin >> q;
+    while(q--){
+        char c;
+        cout << "Enter the query " << endl;
+        cin >> c;
+        //fetch
+        cout << mpp[c] << endl;
     }
     return 0;
 }
