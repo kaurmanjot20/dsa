04dcc62cbc66e225ff3f626f2b370ac29604568e
Manjot Kaur <129861321+kaurmanjot20@users.noreply.github.com>
2024-11-22 14:41:35 +0530

    Create inOrderRecur.cpp

---
+++ inOrderRecur.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<>();
+        if(root==null) return output;
+        output.addAll(inorderTraversal(root.left));
+        output.add(root.val);
+        output.addAll(inorderTraversal(root.right));
+        return output;
+    }
+}
